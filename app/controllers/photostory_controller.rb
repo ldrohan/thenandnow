a27777b55432f1c350954ee5a6b4f371b4e95d39
ldrohan <drohanluke@gmail.com>
2014-11-03 12:31:11 -0800

    devise working, carrierwave gem installed

--- app/controllers/photostory_controller.rb
+++ app/controllers/photostory_controller.rb
@@ -1,4 +1,5 @@
 class PhotostoryController < ApplicationController
+	before_action :authenticate_user!
 	def index
 		# Users stories
 	end	
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'photostory#index'
 
   get 'photostory/index' => 'photostory#index'
