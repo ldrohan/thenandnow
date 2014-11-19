7aa38d8f26c6dfb9c8da0cdcc1c20d218a84efe1
ldrohan <drohanluke@gmail.com>
2014-11-19 15:00:44 -0800

    googlemaps lazy loading working, finally

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,26 +1,52 @@
-function initialize() {
-  var mapOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  $('.picModal').on('show.bs.modal', function () {
-    map.setCenter(new google.maps.LatLng(42.3605336, -72.6362989));
-    google.maps.event.trigger(map, 'resize');
+$(document).ready(function(){
+  var map;        
+              var myCenter=new google.maps.LatLng(53, -1.33);
+  var marker=new google.maps.Marker({
+      position:myCenter
   });
 
-}
+  function initialize() {
+    var mapProp = {
+        center:myCenter,
+        zoom: 14,
+        draggable: false,
+        scrollwheel: false,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    
+    map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+    marker.setMap(map);
+      
+    google.maps.event.addListener(marker, 'click', function() {
+        
+      infowindow.setContent(contentString);
+      infowindow.open(map, marker);
+      
+    }); 
+  };
+  google.maps.event.addDomListener(window, 'load', initialize);
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-}
+  google.maps.event.addDomListener(window, "resize", resizingMap());
 
-window.onload = loadScript;
+  $('.picModal').on('show.bs.modal', function() {
+     //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
+     resizeMap();
+  })
+
+  function resizeMap() {
+     if(typeof map =="undefined") return;
+     setTimeout( function(){resizingMap();} , 400);
+  }
+
+  function resizingMap() {
+     if(typeof map =="undefined") return;
+     var center = map.getCenter();
+     google.maps.event.trigger(map, "resize");
+     map.setCenter(center); 
+  }
+
+
+
+});
+
 
