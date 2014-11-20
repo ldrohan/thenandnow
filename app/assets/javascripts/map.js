ecf4678a1176340323e441bffb08f5c768f60230
ldrohan <drohanluke@gmail.com>
2014-11-20 15:20:01 -0800

    geocoding address working

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.map').each(function() {
+    console.log(this.id);
     var container = this;  
-    var map;        
+    var map;
+    var geoCoder = new google.maps.Geocoder(); 
+    var address = this.id.split("%20").join(" ");       
     var myCenter = new google.maps.LatLng(37.794294, -122.409821);
     var marker = new google.maps.Marker({
         position:myCenter
@@ -10,18 +13,31 @@ $(document).ready(function(){
     var mapStyle = [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#EBE5E0"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]}]
 
     function initialize() {
+
+      geoCoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+          });
+        } else {
+          position: myCenter
+        }
+      });
+    
       var mapProp = {
-          center:myCenter,
+          center: myCenter,
           zoom: 14,
           draggable: false,
           scrollwheel: false,
-          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
           styles: mapStyle
       };
       
       map=new google.maps.Map(container,mapProp);
       marker.setMap(map);
-    };
+    }; //end initialize// 
  
     google.maps.event.addDomListener(window, 'load', initialize);
 
