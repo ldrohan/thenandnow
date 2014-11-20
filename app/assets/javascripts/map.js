3cd1a7c5655152229af2ef1ab6e36210578935a7
ldrohan <drohanluke@gmail.com>
2014-11-20 14:33:21 -0800

    google map working with each loop

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,54 +1,52 @@
 $(document).ready(function(){
-  var map;        
-  var myCenter=new google.maps.LatLng(37.794294, -122.409821);
-  var marker=new google.maps.Marker({
-      position:myCenter
-  });
+  $('.map').each(function() {
+    var container = this;  
+    var map;        
+    var myCenter = new google.maps.LatLng(37.794294, -122.409821);
+    var marker = new google.maps.Marker({
+        position:myCenter
+    });
 
-  var numOfModals = document.getElementsByClassName('picModal').length;
 
-  for (var i = 0; i <= numOfModals.length; i++) {}
+    var mapStyle = [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#EBE5E0"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]}]
 
-  function initialize() {
-    var mapProp = {
-        center:myCenter,
-        zoom: 14,
-        draggable: false,
-        scrollwheel: false,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
+    function initialize() {
+      var mapProp = {
+          center:myCenter,
+          zoom: 14,
+          draggable: false,
+          scrollwheel: false,
+          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          styles: mapStyle
+      };
+      
+      map=new google.maps.Map(container,mapProp);
+      marker.setMap(map);
     };
-    
-    map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
-    marker.setMap(map);
-      
-    google.maps.event.addListener(marker, 'click', function() {
-        
-      infowindow.setContent(contentString);
-      infowindow.open(map, marker);
-      
-    }); 
-  };
-  google.maps.event.addDomListener(window, 'load', initialize);
+ 
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-  google.maps.event.addDomListener(window, "resize", resizingMap());
+    google.maps.event.addDomListener(window, "resize", resizingMap());
 
-  $('.picModal').on('show.bs.modal', function() {
-     //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
-     resizeMap();
-  })
 
-  function resizeMap() {
-     if(typeof map =="undefined") return;
-     setTimeout( function(){resizingMap();} , 400);
-  }
+    $('.picModal').on('show.bs.modal', function() {
+       //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
+       resizeMap();
+    })
 
-  function resizingMap() {
-     if(typeof map =="undefined") return;
-     var center = map.getCenter();
-     google.maps.event.trigger(map, "resize");
-     map.setCenter(center); 
-  }
+    function resizeMap() {
+       if(typeof map =="undefined") return;
+       setTimeout( function(){resizingMap();} , 400);
+    }
 
+    function resizingMap() {
+       if(typeof map =="undefined") return;
+       var center = map.getCenter();
+       google.maps.event.trigger(map, "resize");
+       map.setCenter(center); 
+    }
+  
+  });  
 });
 
 
